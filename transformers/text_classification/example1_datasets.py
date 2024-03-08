14a221e9c2cb6aace8be04eafde58d3e83654c1d
Anupam <anupam.kaul@yahoo.com>
2024-03-08 15:50:07 -0800

    Install and run conda and jupyter on mac and begin text classification with LLMs

---
+++ transformers/text_classification/example1_datasets.py
@@ -0,0 +1,7 @@
+from datasets import list_datasets
+
+all_datasets = list_datasets()
+print(f"There are {len(all_datasets)} datasets currently available on the hub")
+print(f"The first 10 are: {all_datasets[:10]}")
+
+
