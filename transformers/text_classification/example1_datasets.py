ec0e194c6622b48e39053e5addc9a22aa665403e
Anupam <anupam.kaul@yahoo.com>
2024-03-11 17:44:09 -0700

    Invent and simplify : separate datasets example from setup matplotlib
    Fetching data sets from hub works :)
    
    python example1_datasets.py
    
    There are 118295 datasets currently available on the hub
    The first 10 are: ['acronym_identification', 'ade_corpus_v2', 'UCLNLP/adversarial_qa', 'aeslc', 'afrikaans_ner_corpus', 'ag_news', 'allenai/ai2_arc', 'air_dialogue', 'ajgt_twitter_ar', 'allegro_reviews']
    
    .. and we are ready to go with examples !

--- transformers/text_classification/example1_datasets.py
+++ transformers/text_classification/example1_datasets.py
@@ -1,15 +1,8 @@
-from utils import *
-setup_chapter()
-
-print(" done with setup_chapter\n")
-
-'''
 from datasets import list_datasets
 
 all_datasets = list_datasets()
 print(f"There are {len(all_datasets)} datasets currently available on the hub")
 print(f"The first 10 are: {all_datasets[:10]}")
-'''
 
 
 
