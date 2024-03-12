e741e6deeb6ee2fdd84a3a1e76aae4f8e5e3792a
Anupam <anupam.kaul@yahoo.com>
2024-03-11 18:05:01 -0700

    Inspect dataset acronym_identification (print variable prints its type)
    python example1_datasets.py
    Using custom data configuration default
    Reusing dataset acronym_identification (/Users/anupkaul/.cache/huggingface/datasets/acronym_identification/default/1.0.0/e84facf8db848a4c7aa58addbebaf8a161c4146ca367e923ca972673cc915425)
    100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 3/3 [00:00<00:00, 57.50it/s]
    DatasetDict({
        train: Dataset({
            features: ['id', 'tokens', 'labels'],
            num_rows: 14006
        })
        validation: Dataset({
            features: ['id', 'tokens', 'labels'],
            num_rows: 1717
        })
        test: Dataset({
            features: ['id', 'tokens', 'labels'],
            num_rows: 1750
        })
    })

--- transformers/text_classification/examples/example1_datasets.py
+++ transformers/text_classification/examples/example1_datasets.py
@@ -5,9 +5,11 @@ to see what datasets are available on the hub
 
 from datasets import list_datasets
 
+'''
 all_datasets = list_datasets()
 print(f"There are {len(all_datasets)} datasets currently available on the hub")
 print(f"The first 50 are: {all_datasets[:50]}")
+'''
 
 '''
 Since each dataset has a name, let's load up one and see what it has
@@ -25,6 +27,7 @@ emotions = load_dataset("emotion")
 '''
 
 acronym_identification = load_dataset("acronym_identification")
+print(acronym_identification)
 
 
 
