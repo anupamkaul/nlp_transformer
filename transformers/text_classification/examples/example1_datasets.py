1bb82bd0d93daacd2d590bec2d5f27e617b98f90
Anupam <anupam.kaul@yahoo.com>
2024-03-11 17:55:54 -0700

    Add example code to query datasets and print the first 50, and try loading the arxiv dataset (dump in example1.out)

---
+++ transformers/text_classification/examples/example1_datasets.py
@@ -0,0 +1,27 @@
+'''
+Use Datasets to dowload data from Hugging Face hub. We can use list_datasets()
+to see what datasets are available on the hub
+'''
+
+from datasets import list_datasets
+
+all_datasets = list_datasets()
+print(f"There are {len(all_datasets)} datasets currently available on the hub")
+print(f"The first 50 are: {all_datasets[:50]}")
+
+'''
+Since each dataset has a name, let's load up one and see what it has
+(see example1.out for the first 50 datasets)
+'''
+
+from datasets import load_dataset
+
+arxiv_dataset = load_dataset("arxiv_dataset")
+
+print(arxiv_dataset)
+
+
+
+
+
+
