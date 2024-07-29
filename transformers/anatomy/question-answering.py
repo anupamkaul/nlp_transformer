5a26fcacd9a3747b7f2651b4d5f336407191a9e0
Anupam <anupam.kaul@yahoo.com>
2024-07-29 15:21:03 -0700

    Change the text to (1) replace megatron with gashtuk and (2) indicate package was empty
    Both questions are answered correctly, although for extractive, the transformer
    copies the text from the sentence itself, including the gramattical error
    in the word "ordered"
    
    python question-answering.py
    2024-07-29 15:20:29.151680: I tensorflow/core/platform/cpu_feature_guard.cc:210] This TensorFlow binary is optimized to use available CPU instructions in performance-critical operations.
    To enable the following instructions: AVX2 AVX512F AVX512_VNNI FMA, in other operations, rebuild TensorFlow with the appropriate compiler flags.
    No model was supplied, defaulted to distilbert-base-cased-distilled-squad (https://huggingface.co/distilbert-base-cased-distilled-squad)
          score  start  end                               answer
    0  0.488333    325  360  resend the Optimus Prime I ordereed

--- transformers/anatomy/question-answering.py
+++ transformers/anatomy/question-answering.py
@@ -1,6 +1,8 @@
 
 
-text = """Dear Amazon, last week I ordered an Optimus Prime action figure from your online store in Germany. Unfortunately, when I opened the package, I discoverd to my horror that I had been sent an action figure of Megatron instead ! As a lifelong enemy of Decepticons, I hope you can understand my dilemma. To resolve the issue, I demand an exchange of Megatron for the Optimus Prime figure I ordereed. Enclosed are copies of my records concerning this purchase. I expect to hear from your soon. Sincerely, Bumblebee. """
+# text = """Dear Amazon, last week I ordered an Optimus Prime action figure from your online store in Germany. Unfortunately, when I opened the package, I discoverd to my horror that I had been sent an action figure of Gashtuk instead ! As a lifelong enemy of Decepticons, I hope you can understand my dilemma. I really feel stuck! To resolve the issue, I demand an exchange of Gashtuk for the Optimus Prime figure I ordereed. Enclosed are copies of my records concerning this purchase. I expect to hear from your soon. Sincerely, Bumblebee. """
+
+text = """Dear Amazon, last week I ordered an Optimus Prime action figure from your online store in Germany. Unfortunately, when I opened the package, I discoverd to my horror that the package was empty ! As a lifelong enemy of Decepticons, I hope you can understand my dilemma. I really feel stuck! To resolve the issue, I demand you resend the Optimus Prime I ordereed, this time with the actual Optimus Prime. Enclosed are copies of my records concerning this purchase. I expect to hear from your soon. Sincerely, Bumblebee. """
 
 from transformers import pipeline
 reader = pipeline("question-answering")
