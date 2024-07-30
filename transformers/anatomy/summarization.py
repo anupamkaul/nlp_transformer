a28e8bbae9d8455585209d87fce5444980ff64e1
Anupam <anupam.kaul@yahoo.com>
2024-07-30 09:03:43 -0700

    removing the extra line makes the summarization better (why?)
    python summarization.py
    2024-07-30 09:02:49.361723: I tensorflow/core/platform/cpu_feature_guard.cc:210] This TensorFlow binary is optimized to use available CPU instructions in performance-critical operations.
    To enable the following instructions: AVX2 AVX512F AVX512_VNNI FMA, in other operations, rebuild TensorFlow with the appropriate compiler flags.
    No model was supplied, defaulted to sshleifer/distilbart-cnn-12-6 (https://huggingface.co/sshleifer/distilbart-cnn-12-6)
    Special tokens have been added in the vocabulary, make sure the associated word embeddings are fine-tuned or trained.
    Your min_length=56 must be inferior than your max_length=45.
     Bumblebee ordered an Optimus Prime action figure from your online store in Germany. Unfortunately, when I opened the package, I discoveredd to my horror that I had been sent an action figure of Gasht

--- transformers/anatomy/summarization.py
+++ transformers/anatomy/summarization.py
@@ -1,6 +1,9 @@
 
 
-text = """Dear Amazon, last week I ordered an Optimus Prime action figure from your online store in Germany. Unfortunately, when I opened the package, I discoverd to my horror that I had been sent an action figure of Gashtuk instead ! As a lifelong enemy of Decepticons, I hope you can understand my dilemma. I really feel stuck! To resolve the issue, I demand an exchange of Gashtuk for the Optimus Prime figure I ordereed. Enclosed are copies of my records concerning this purchase. I expect to hear from your soon. Sincerely, Bumblebee. """
+#text = """Dear Amazon, last week I ordered an Optimus Prime action figure from your online store in Germany. Unfortunately, when I opened the package, I discoverd to my horror that I had been sent an action figure of Gashtuk instead ! As a lifelong enemy of Decepticons, I hope you can understand my dilemma. I really feel stuck! To resolve the issue, I demand an exchange of Gashtuk for the Optimus Prime figure I ordereed. Enclosed are copies of my records concerning this purchase. I expect to hear from your soon. Sincerely, Bumblebee. """
+
+# no 'I really feel stuck'
+text = """Dear Amazon, last week I ordered an Optimus Prime action figure from your online store in Germany. Unfortunately, when I opened the package, I discoverd to my horror that I had been sent an action figure of Gashtuk instead ! As a lifelong enemy of Decepticons, I hope you can understand my dilemma.To resolve the issue, I demand an exchange of Gashtuk for the Optimus Prime figure I ordereed. Enclosed are copies of my records concerning this purchase. I expect to hear from your soon. Sincerely, Bumblebee. """
 
 #text = """Dear Amazon, last week I ordered an Optimus Prime action figure from your online store in Germany. Unfortunately, when I opened the package, I discoverd to my horror that the package was empty ! As a lifelong enemy of Decepticons, I hope you can understand my dilemma. I really feel stuck! To resolve the issue, I demand you resend the Optimus Prime I ordereed, this time with the actual Optimus Prime. Enclosed are copies of my records concerning this purchase. I expect to hear from your soon. Sincerely, Bumblebee. """
 
@@ -8,8 +11,9 @@ from transformers import pipeline
 summarizer = pipeline("summarization")
 
 import pandas as pd
-outputs = summarizer(text, max_length=56, clean_up_tokenization_spaces=True)
+outputs = summarizer(text, max_length=45, clean_up_tokenization_spaces=True)
 print(outputs[0]['summary_text'])
+#print(outputs)
 
 
 
